f77b4eb32ca0c444c1e577532b4ff7e85ae91435
Kristina <kristina.ferm@gmail.com>
2023-09-05 17:15:08 +0200

    Temporary site information

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <template>
   <div class="femme-fusion">
-    <Menu />
-    <Hero />
+    <img class="logo" src="./assets/logo/logo-white.svg" />
+    <div class="tag">
+      The femme fatales of burlesque - burlesque showgrupp baserad i Linköping
+    </div>
+    <div class="info">
+      <div>
+        Medan vi arbetar med att färdigställa vår hemsida funderar du på när du vill boka in oss för en show!
+      </div>
+      <div class="info__email">
+        <a href="mailto:info@femmefusion.se">info@femmefusion.se</a>
+      </div>
+      <div class="info__social">
+        <a href="https://www.instagram.com/femmefusionofficial/" target="_blank">
+          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0,0,256,256" width="256px" height="256px"><g transform=""><g fill="#ffffff" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><g transform="scale(5.33333,5.33333)"><path d="M16.5,5c-6.33361,0 -11.5,5.16639 -11.5,11.5v15c0,6.33276 5.16621,11.5 11.5,11.5h15c6.33294,0 11.5,-5.16706 11.5,-11.5v-15c0,-6.33379 -5.16724,-11.5 -11.5,-11.5zM16.5,8h15c4.71124,0 8.5,3.78779 8.5,8.5v15c0,4.71106 -3.78894,8.5 -8.5,8.5h-15c-4.71221,0 -8.5,-3.78876 -8.5,-8.5v-15c0,-4.71239 3.78761,-8.5 8.5,-8.5zM34,12c-1.105,0 -2,0.895 -2,2c0,1.105 0.895,2 2,2c1.105,0 2,-0.895 2,-2c0,-1.105 -0.895,-2 -2,-2zM24,14c-5.50482,0 -10,4.49518 -10,10c0,5.50482 4.49518,10 10,10c5.50482,0 10,-4.49518 10,-10c0,-5.50482 -4.49518,-10 -10,-10zM24,17c3.88318,0 7,3.11682 7,7c0,3.88318 -3.11682,7 -7,7c-3.88318,0 -7,-3.11682 -7,-7c0,-3.88318 3.11682,-7 7,-7z"></path></g></g></g></svg>
+        </a>
+        <a href="https://www.facebook.com/femmefusionofficial" target="_blank">
+          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0,0,256,256" width="256px" height="256px"><g transform=""><g fill-opacity="0" fill="#be0e0e" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><path d="M0,256v-256h256v256z" id="bgRectangle"></path></g><g fill="#ffffff" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><g transform="scale(4,4)"><path d="M48,7h-32c-4.418,0 -8,3.582 -8,8v32c0,4.418 3.582,8 8,8h17v-17h-6v-7h6v-5c0,-7 4,-11 10,-11c3.133,0 5,1 5,1v6h-4c-2.86,0 -4,2.093 -4,4v5h7l-1,7h-6v17h8c4.418,0 8,-3.582 8,-8v-32c0,-4.418 -3.582,-8 -8,-8z"></path></g></g></g></svg>
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 
 
 <script setup lang="ts">
-import Hero from './components/hero/hero.vue';
-import Menu from './components/menu/menu.vue';
-
 </script>
 
 <style lang="scss">
