bb67a26b59dcd441f68826761817a7502a984d4a
Kristina Ferm <kristina.ferm@knowit.se>
2023-09-20 20:35:49 +0200

    Testamonials

---
+++ src/components/testamonials/Testamonials.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="testamonials">
+    <div class="testamonials__quote">
+      "Why hide my amazing artistic clawing skills? or eat a rug and furry furry hairs everywhere oh no human coming lie on counter don't get off counter."
+    </div>
+    <div class="testamonials__quote testamonials__quote--right"> 
+      "Meoooow commence midnight zoomies."
+    </div>
+    <div class="testamonials__quote">
+      "I show my fluffy belly but it's a trap! if you pet it i will tear up your hand purr for no reason. Steal the warm chair right after you get up."
+    </div>
+  </div>
+</template>
+  
+<script setup lang="ts">
+import { ref } from 'vue';
+
+</script>
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,17 +2,23 @@
   <div class="home content">
     <section>
       <h1>Femme Fusion</h1>
-      With tail in the air. Gnaw the corn cob sleep over your phone and make cute snoring noises, stare at guinea pigs try to jump onto window and fall while scratching at wall or i will ruin the couch with my claws and kitty kitty pussy cat doll. Lie on your belly and purr when you are asleep good now the other hand, too run off table persian cat jump eat fish soft kitty warm kitty little ball of furr for eat the rubberband hey! 
+      With tail in the air. Gnaw the corn cob sleep over your phone and make cute snoring noises, stare at guinea pigs try
+      to jump onto window and fall while scratching at wall or i will ruin the couch with my claws and kitty kitty pussy
+      cat doll. Lie on your belly and purr when you are asleep good now the other hand, too run off table persian cat jump
+      eat fish soft kitty warm kitty little ball of furr for eat the rubberband hey!
     </section>
-    <Image 
-      path="../../src/assets/images/louboutin.jpg" 
-      alt="Louboutinsko i svart lack mot en parkeringshusvägg"
+    <Image path="../../src/assets/images/louboutin.jpg" alt="Louboutinsko i svart lack mot en parkeringshusvägg"
       :fullWidth="true" />
+    <section>
+      <h1>Sagt om oss</h1>
+      <Testamonials />
+    </section>
   </div>
 </template>
 
 <script setup lang="ts">
 import { onMounted, computed } from 'vue';
 import Image from '../../components/image/Image.vue';
+import Testamonials from '../../components/testamonials/Testamonials.vue';
 
 </script>
