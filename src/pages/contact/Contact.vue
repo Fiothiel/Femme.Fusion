0af5042850b7b0536c5a3fc8b94058b6db4c5e27
Kristina Ferm <kristina.ferm@knowit.se>
2023-09-21 20:43:12 +0200

    Contact form and button styling

--- src/pages/contact/Contact.vue
+++ src/pages/contact/Contact.vue
@@ -1,14 +1,29 @@
 <template>
-  <Page heading="Kontakta oss" preamble="" image="" content="" />
-  <form class="contact-form" ref="form" @submit.prevent="sendEmail">
-    <label>Name</label>
-    <input type="text" name="user_name" v-model="name" />
-    <label>Email</label>
-    <input type="email" name="user_email" v-model="email" />
-    <label>Message</label>
-    <textarea name="message" v-model="message"></textarea>
-    <input type="submit" value="Send" />
-  </form>
+  <div class="contact content">
+    <h1>Kontakta oss</h1>
+    <div>
+      <p>Catch mouse and gave it as a present as lick i the shoes cat walks in keyboard for cat slap dog in face cat
+        meoooow i iz master of hoomaan, not hoomaan master of i, oooh damn dat dog sleep in the bathroom sink and refuse
+        to
+        leave cardboard box. Spread kitty litter all over house spend six hours per day washing, but still have a crusty
+        butthole yet pee in the shoe white cat sleeps on a black shirt, sleep. Purr for no reason man running from cops
+        stops to pet cats, goes to jail and jump on fridge lick master's hand at first then bite because im moody yet lick
+        yarn hanging out of own butt so eats owners hair then claws head yet i do no work yet get food, shelter, and lots
+        of
+        stuff just like man who lives with us.
+      </p>
+
+      <form class="contact__form" ref="form" @submit.prevent="sendEmail">
+        <label>Namn</label>
+        <input type="text" name="user_name" v-model="name" />
+        <label>Email</label>
+        <input type="email" name="user_email" v-model="email" />
+        <label>Meddelande</label>
+        <textarea name="message" v-model="message"></textarea>
+        <button type="submit" class="button">Skicka</button>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -41,3 +56,4 @@ const sendEmail = () => {
     });
 };
 </script>
+>
