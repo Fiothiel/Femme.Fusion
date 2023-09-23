6cfc2b90e0e7af51628ecdb7192b65410c6f0f00
Kristina Ferm <kristina.ferm@knowit.se>
2023-09-23 21:43:36 +0200

    Social icons hover

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -37,7 +37,7 @@
         </g>
       </svg>
     </a>
-    <a href="mailto:info@femmefusion.se" class="footer__icon">
+    <a href="mailto:info@femmefusion.se" class="footer__icon footer__icon--mail">
       <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="256"
         height="256" viewBox="0 0 256 256" xml:space="preserve">
 
