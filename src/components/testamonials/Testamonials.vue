ece8744423b21a0553f732c8756d5a40fa0b81dc
Kristina <kristina.ferm@gmail.com>
2024-04-17 15:07:56 +0200

    Course data and quote

--- src/components/testamonials/Testamonials.vue
+++ src/components/testamonials/Testamonials.vue
@@ -2,6 +2,12 @@
   <div class="testamonials">
     <div class="testamonials__quote testamonials__quote--right">
       <div>
+        "Femme Fusion levererar energi och glädje som tänder även den mest svårflörtade publik. På vårt event med närmare 500 gäster fick de stående ovationer!"
+      </div>
+      <div class="testamonials__from">- Kent Renåker, System Q event & expo 2024</div>
+    </div>
+    <div class="testamonials__quote">
+      <div>
         "Femme Fusion är en proffsig och engagerad dansgrupp som verkligen
         levererar efter kundens önskemål. Vi på HSB Östergötland hade nöjet att
         anlita dem i samband med vår personalfest. Deras shownummer satte ribban
@@ -9,13 +15,7 @@
         rekommendera Femme Fusion för att sätta ribban på er fest!"
       </div>
       <div class="testamonials__from">- HSB Östergötland, show 2023</div>
-    </div>
-    <div class="testamonials__quote">
-      <div>
-        "Väldigt bra workshop, bra tempo, pedagogiskt upplagt och superkul"
-      </div>
-      <div class="testamonials__from">- deltagare burlesqueworkshop 2022</div>
-    </div>
+    </div>    
     <div class="testamonials__quote testamonials__quote--right">
       <div>
         "Välkomnande och uppmuntrande miljö. Här får man va som man är och bara
