f8c0ccc0f1268140ddcedbb9c70405a050d98d00
Kristina <kristina.ferm@gmail.com>
2024-06-05 21:12:36 +0200

    Tag line

--- src/components/hero/hero.vue
+++ src/components/hero/hero.vue
@@ -2,7 +2,7 @@
   <div class="hero">
     <div class="hero__tag-logo">      
       <img class="hero__logo" src="../../assets/logo/logo-white.svg" />
-      <span class="hero__tag">The femme fatales of burlesque</span>
+      <span class="hero__tag">Experience the extraordinary</span>
     </div>
     <div class="hero__overlay"></div>
     <img 
